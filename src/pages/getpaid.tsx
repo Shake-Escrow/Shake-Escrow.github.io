02912543b9b8cd3a0b3d846aa7950b876ac2a296
Lynnabella <cmacreatives@gmail.com>
2025-06-16 15:35:43 -0700

    Send payments get paid cards fix V3

--- src/pages/getpaid.tsx
+++ src/pages/getpaid.tsx
@@ -23,7 +23,7 @@ const GetPaid: React.FC = () => {
       </Section>
 
       <div className="mt-6">
-        <div className="max-w-5xl mx-auto flex flex-col gap-y-7">
+        <div className="max-w-5xl mx-auto flex flex-col gap-y-7 px-6 md:px-0">
           {/* First card: Keep More of What You Earn */}
           {(() => {
             const section = siteContent.getPaid.sections[0];
@@ -51,7 +51,7 @@ const GetPaid: React.FC = () => {
               if (section.headline.replace(/\n/g, ' ') === 'Adapts to You') imageSrc = '/images/03_adapts to you.png';
               if (section.headline.replace(/\n/g, ' ') === 'Reduce Risk by Building Trust') imageSrc = '/images/02_reduce risk by building trust.png';
               return (
-                <div key={section.headline} className={`bg-[#e6e9ed] rounded-2xl py-8 px-6 md:px-12 md:py-12 flex flex-col items-start text-left h-full ${idx === 0 ? ' md:mb-0' : 'mt-18 md:mt-0'}`}>
+                <div key={section.headline} className={`bg-[#e6e9ed] rounded-2xl py-8 px-6 md:px-12 md:py-12 flex flex-col items-start text-left h-full ${idx === 0 ? ' md:mb-0' : 'mt-16 md:mt-0'}`}>
                   <img src={imageSrc} alt={section.headline} className="w-full h-43 object-cover rounded-2xl " />
                   <h3 className="font-display font-bold text-2xl md:text-3xl text-secondary-dark mb-4" style={{letterSpacing: '-0.03em'}}>{section.headline.split('\n').map((line, idx) => (<React.Fragment key={idx}>{line}{idx < section.headline.split('\n').length - 1 && <br />}</React.Fragment>))}</h3>
                   <p className="text-description mb-6 text-[#2d3440] opacity-90" style={{fontFamily: 'Lexend Deca, sans-serif'}}>{section.description}</p>
